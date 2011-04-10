31024f97065eb6f07119d7b7580d7c1c83071916
Scott Farrar <Scott@badboy.myhome.westell.com>
2011-04-10 09:45:56 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kawapanan::Application.config.secret_token = 'ae3e123b8bead640ab51d759dcffa814ce59b701a72a26ee1b64e13a188b773e7848a1d11233f4889461527e3e9fe7406320a8181f3b4006f017586473286d8f'
