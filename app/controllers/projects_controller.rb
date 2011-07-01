dd5b72fa4969d726f8890cec65329e426416db86
Kelly Peterson <burgersmoke@gmail.com>
2011-07-01 16:06:37 -0600

    Trying another means of dealing with the JSON-based (salt) authentication

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,7 @@ class ProjectsController < ApplicationController
 
 	# TODO kelly -- how should we get beyond authentication for query?
 	# should we do a custom authentication in 'query' or should we add something to 'signed_in?' in sessions_helper.rb?
-	before_filter :authenticate, :except => [:query]
+	before_filter :authenticate, :except => [:query, :create_salt]
 	before_filter :authenticate_salt, :only => [:query]
 	
 	respond_to :html, :json
@@ -52,6 +52,31 @@ class ProjectsController < ApplicationController
       flash[:success] = "Successfully created "+@project.name
       redirect_to @project
      end
+	 
+	 def create_salt
+		# TODO kelly -- can I get around duplicating this again?
+		@language = Language.find_by_name(params[:project][:language])
+		
+		@language_id = -1
+		if @language
+			@language_id = @language.id
+		else
+			flash[:error] = "Could not find language.  Are languages loaded in the DB? "
+		end
+		
+		@project = current_user.projects.build(:name => params[:project][:name], 
+			:description => params[:project][:description],	
+			:activity => params[:project][:activity],
+			:language_id => @language_id,
+			:user_id => current_user.id
+		)
+
+		
+     if @project.save
+      flash[:success] = "Successfully created "+@project.name
+      redirect_to @project
+	 end
+	 
   end
 
 
