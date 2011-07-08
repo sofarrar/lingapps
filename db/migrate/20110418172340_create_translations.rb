bc4f28c9a4b12711a98d0defd3e688124f066c5b
Kelly Peterson <burgersmoke@gmail.com>
2011-07-07 21:06:48 -0600

    Fixing up translations so that each one is owned by a project

--- db/migrate/20110418172340_create_translations.rb
+++ db/migrate/20110418172340_create_translations.rb
@@ -1,11 +1,13 @@
 class CreateTranslations < ActiveRecord::Migration
   def self.up
     create_table :translations do |t|
+		t.integer :project_id
       t.references :source
       t.references :target
 
       t.timestamps
     end
+	add_index :translations, :project_id
   end
 
   def self.down
